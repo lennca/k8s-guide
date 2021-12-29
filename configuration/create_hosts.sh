1779a3b32eccc86ca55489b031b90fd48e418e6c
Caesar Lennartsson <caesar.1999@hotmail.com>
2021-12-29 23:05:49 +0100

    Update script to create config file too

--- configuration/create_hosts.sh
+++ configuration/create_hosts.sh
@@ -1,25 +1,24 @@
 #!/bin/bash
 
-file=hosts
+hosts_file=hosts
+config_file=ansible.cfg
 user=ubuntu
 master_ip=<master-ip>
 worker_ip=<slave-ip>
 key_path=<path-to-key>
 
+# Create hosts file
 # If hosts file already exists, remove
-if [ -e ./$file ]; then
-  rm ./$file
+if [ -e ./$hosts_file ]; then
+  rm ./$hosts_file
 fi
 
 # Write to hosts file
-
-cat > ./$file <<EOL
+cat > ./$hosts_file <<EOL
 [masters]
-#${master-name} ansible_host=${master-ip} ansible_user=ubuntu
 k8-master ansible_host=$master_ip ansible_user=$user
 
 [workers]
-#${worker-name} ansible_host=${worker-ip} ansible_user=ubuntu
 k8-worker ansible_host=$worker_ip ansible_user=$user
 
 #[bastions]
@@ -29,4 +28,22 @@ k8-worker ansible_host=$worker_ip ansible_user=$user
 ansible_ssh_common_args='-o StrictHostKeyChecking=no -o ProxyCommand="ssh -W %h:%p -q -i $key_path $user@$master_ip"'
 ansible_ssh_user=$user
 ansible_python_interpreter=/usr/bin/python3
+EOL
+
+
+# Create ansible.cfg file
+# If hosts file already exists, remove
+if [ -e ./$config_file ]; then
+  rm ./$config_file
+fi
+
+cat > ./$config_file <<EOL
+[defaults]
+inventory = ./$hosts_file
+remote_user = $user
+private_key_file = $key_path
+command_warnings=False
+deprecation_warnings=False
+# To ignore known_hosts issue
+host_key_checking=False
 EOL
